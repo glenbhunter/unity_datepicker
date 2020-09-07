e425ebc2f827c87033dd4d042ac953cfb5b68813
Glen Hunter <hunter.glen@gmail.com>
2020-09-07 23:43:18 +0100

    + glen hunter tweening libary + cleaned up fading functionality

---
+++ Assets/DatePicker/Scripts/Calender.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GlenHunter
+{
+    public class Calender : MonoBehaviour
+    {
+        public DayOfWeek FirstDayOfWeek;
+
+        [SerializeField] Text m_DateHeading;
+
+        public List<Text> DaysOfWeekLabels;
+        public List<CalenderButton> CalenderButtons;
+
+        private int m_ButtonClickCount;
+        private DateTime? m_StartDate;
+        private DateTime? m_EndDate;
+
+        private CalenderButton m_StartDateBTN;
+        private CalenderButton m_EndDateBTN;
+
+
+        private DateTime m_CurrentCalenderDate;
+
+
+        private void Start()
+        {
+            m_CurrentCalenderDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+
+            SetupCalenderGrid(m_CurrentCalenderDate);
+        }
+
+        public void SetupCalenderGrid(DateTime calenderDate)
+        {
+            int calenderMonth = calenderDate.Month;
+            int calenderYear = calenderDate.Year;
+
+            Update_CurrentDateHeading(calenderMonth, calenderYear);
+
+            Update_DaysOfWeekHeadings(calenderMonth, calenderYear);
+
+            Update_CalenderButtons(calenderMonth, calenderYear);
+        }
+
+        private void Update_CurrentDateHeading(int calenderMonth, int calenderYear)
+        {
+            string month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(calenderMonth);
+            m_DateHeading.text = month.ToUpper() + " " + calenderYear;
+        }
+
+        private void Update_DaysOfWeekHeadings(int calenderMonth, int calenderYear)
+        {
+            DateTime startDate = new DateTime(calenderYear, calenderMonth, 1);
+            DayOfWeek firstDayOfMonth = startDate.DayOfWeek;
+
+
+            // Used for mon-sun labels
+            int startingIndex = (int)FirstDayOfWeek;
+
+            for (int i = 0; i < 7; i++)
+            {
+                DaysOfWeekLabels[i].text = ((DayOfWeek)startingIndex).ToString().Remove(3).ToUpper();
+
+                startingIndex++;
+
+                if (startingIndex > 6)
+                    startingIndex = 0;
+            }
+        }
+
+        private void Update_CalenderButtons(int calenderMonth, int calenderYear)
+        {
+            DateTime startDate = new DateTime(calenderYear, calenderMonth, 1);
+            DayOfWeek firstDayOfMonth = startDate.DayOfWeek;
+
+            int daysBefore = firstDayOfMonth - FirstDayOfWeek;
+            startDate = startDate.AddDays(-daysBefore);
+
+            for (int i = 0; i < 42; i++)
+            {
+                CalenderButtons[i].Setup(startDate, (startDate.Month == calenderMonth), this);
+
+                if (m_StartDate != null && startDate == m_StartDate)
+                {
+                    m_StartDateBTN = CalenderButtons[i];
+                }
+                else if (m_EndDateBTN != null && startDate == m_EndDate)
+                {
+                    m_EndDateBTN = CalenderButtons[i];
+                }
+                else if (startDate > m_StartDate && startDate < m_EndDate)
+                {
+                    CalenderButtons[i].Highlight();
+                }
+                else
+                {
+                    CalenderButtons[i].ForceClear();
+                }
+
+                startDate = startDate.AddDays(1);
+            }
+        }
+
+        public void OnClick_NextMonth()
+        {
+            m_CurrentCalenderDate = m_CurrentCalenderDate.AddMonths(1);
+            SetupCalenderGrid(m_CurrentCalenderDate);
+        }
+
+        public void OnClick_PreviousMonth()
+        {
+            m_CurrentCalenderDate = m_CurrentCalenderDate.AddMonths(-1);
+            SetupCalenderGrid(m_CurrentCalenderDate);
+        }
+
+        private void Refresh()
+        {
+            DateTime currentDate = new DateTime(m_CurrentCalenderDate.Year, m_CurrentCalenderDate.Month, 1);
+            DayOfWeek firstDayOfMonth = currentDate.DayOfWeek;
+
+            int daysBefore = firstDayOfMonth - FirstDayOfWeek;
+            currentDate = currentDate.AddDays(-daysBefore);
+            //Debug.Log(startDate);
+
+            for (int i = 0; i < 42; i++)
+            {
+                // one click registered
+                if(m_StartDate != null && currentDate == m_StartDate && m_EndDate == null)
+                {
+                    CalenderButtons[i].ForcePressed(true, false);
+                   
+                }
+            // two clicks have registered
+                // is start date    
+                else if(m_StartDate != null && currentDate == m_StartDate)
+                {
+                    CalenderButtons[i].ForcePressed(false, true);
+                }
+                // is end date
+                else if(m_EndDate != null && currentDate == m_EndDate)
+                {
+                    CalenderButtons[i].ForcePressed(false, false);
+                }
+                // is inbetween
+                else if(currentDate > m_StartDate && currentDate < m_EndDate)
+                {
+                    CalenderButtons[i].IsInBetweenFirstAndSecondSelectedDates();
+                }
+                else
+                {
+                    CalenderButtons[i].ForceClear();
+                }
+
+                currentDate = currentDate.AddDays(1);
+            }
+        }
+
+
+        public void OnCalenderButtonClick(DateTime chosenDate, CalenderButton button)
+        {
+            if(m_EndDate != null)
+            {
+                m_StartDate = null;
+                m_EndDate = null;
+            }
+
+            // assume it's first click
+            if(m_StartDate == null)
+            {
+                m_StartDate = chosenDate;
+                m_StartDateBTN = button;
+                Refresh();
+                return;
+            }
+            else if(chosenDate < m_StartDate)
+            {
+                m_StartDate = chosenDate;
+                m_StartDateBTN = button;
+                Refresh();
+                return;
+            }
+
+            // if we get here we can assume the user is selecting the last date
+            m_EndDate = chosenDate;
+            m_EndDateBTN = button;
+            Refresh();
+               
+            }
+        }
+    }
+}
---
+++ Assets/DatePicker/Scripts/CalenderButton.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace GlenHunter
+{
+    public class CalenderButton : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerExitHandler
+    {
+        private enum State
+        {
+            Normal,
+            Pressed,
+            Hover,
+            Highlighted,
+            IsInBetween,
+        }
+
+        [Header("Normal")]
+        [SerializeField] Color32 m_BTN_Normal_Color = Color.white;
+        [SerializeField] Color32 m_TXT_Normal_Color = new Color32(50, 50, 50, 255);
+
+        [Header("Normal but not part of current calender month")]
+        [SerializeField] Color32 m_BTN_Normal_FadedOut_Color = Color.white;
+        [SerializeField] Color32 m_TXT_Normal_FadedOut_Color = new Color32(50, 50, 50, 100);
+
+        [Header("Hover")]
+        [SerializeField] Color32 m_BTN_Hover_Color = new Color32(45, 152, 255, 150);
+        [SerializeField] Color32 m_TXT_Hover_Color = new Color32(50, 50, 50, 150);
+
+        [Header("Highlight")]
+        [SerializeField] Color32 m_BTN_Highlight_Color = new Color32(45, 152, 255, 255);
+        [SerializeField] Color32 m_TXT_Highlight_Color = new Color32(50, 50, 50, 255);
+
+        [Header("InBetweenHightlight")]
+        [SerializeField] Color32 m_BTN_InBetweenColor = new Color32(45, 152, 255, 255);
+
+        [Header("Pressed")]
+        [SerializeField] Color32 m_BTN_Pressed_Color = new Color32(45, 152, 255, 255);
+        [SerializeField] Color32 m_TXT_Pressed_Color = new Color32(50, 50, 50, 255);
+
+
+        [SerializeField] float m_FadeDuration = .1f;
+
+        [SerializeField] Text m_ButtonText;
+
+        /// <summary>
+        /// Primary image used for calender button for example circle
+        /// </summary>
+        [SerializeField] Image m_Primary_Image;
+        [SerializeField] Image m_SecondaryImage;
+
+        [SerializeField] Sprite m_Secondary_Square_Left;
+        [SerializeField] Sprite m_Secondary_Square_Right;
+        [SerializeField] Sprite m_Secondary_Rectangle_Center;
+
+        private State m_CurrentState;
+
+        private Calender m_Calender;
+        private DateTime m_ButtonDate;
+        private bool m_DayIsInCurrentCalenderMonth;
+
+        public void Setup(DateTime btnDate, bool dayIsInCurrentCalenderMonth, Calender calender)
+        {
+            m_ButtonText.text = btnDate.Day.ToString();
+
+            m_DayIsInCurrentCalenderMonth = dayIsInCurrentCalenderMonth;
+
+            m_ButtonDate = btnDate;
+
+            m_Calender = calender;
+        }
+
+        #region PointerClicks - Button States
+
+        #endregion
+
+        #region OverrideState - Button States
+
+        #endregion
+
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Hover();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            m_Calender.OnCalenderButtonClick(m_ButtonDate, this);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Clear();
+        }
+
+        public void Hover()
+        {
+            if (m_CurrentState == State.Normal)
+            {
+                m_CurrentState = State.Hover;
+
+                FadePrimaryButton(m_BTN_Hover_Color, 0);
+                FadePrimaryButtonText(m_TXT_Hover_Color, 0);
+            }
+        }
+
+        public void Pressed()
+        {
+            if (m_CurrentState != State.Normal && m_CurrentState != State.Pressed)
+            {
+                m_CurrentState = State.Pressed;
+
+                FadePrimaryButton(m_BTN_Pressed_Color, 0);
+                FadePrimaryButtonText(m_TXT_Pressed_Color, 0);
+            }
+        }
+
+        public void Highlight()
+        {
+            if (m_CurrentState != State.Highlighted)
+            {
+                m_CurrentState = State.Highlighted;
+
+                FadePrimaryButton(m_BTN_Highlight_Color, 0);
+                FadePrimaryButtonText(m_TXT_Highlight_Color, 0);
+
+            }
+        }
+
+        public void IsInBetweenFirstAndSecondSelectedDates()
+        {
+            m_CurrentState = State.IsInBetween;
+            m_SecondaryImage.sprite = m_Secondary_Rectangle_Center;
+            FadePrimaryButton(Color.clear, 0);
+            FadeSecondaryImage(m_BTN_InBetweenColor, 0);
+            
+        }
+
+        public void Clear()
+        {
+
+            if (m_CurrentState != State.Normal && m_CurrentState == State.Hover)
+            {
+                m_CurrentState = State.Normal;
+
+                if (m_DayIsInCurrentCalenderMonth)
+                {
+                    FadePrimaryButton(m_BTN_Normal_Color, 0);
+                    FadePrimaryButtonText(m_TXT_Normal_Color, 0);
+                }
+                else
+                {
+                    FadePrimaryButton(m_BTN_Normal_FadedOut_Color, 0);
+                    FadePrimaryButtonText(m_TXT_Normal_FadedOut_Color, 0);
+                }
+            }
+        }
+
+
+        public void ForcePressed(bool firstSelection, bool isStartDate)
+        {
+            if (!firstSelection)
+            {
+                m_SecondaryImage.sprite = (isStartDate) ? m_Secondary_Square_Right : m_Secondary_Square_Left;
+
+                FadePrimaryButton(m_BTN_Pressed_Color, 0);
+                FadeSecondaryImage(m_BTN_InBetweenColor, 0);
+                FadePrimaryButtonText(m_TXT_Pressed_Color, 0);
+            }
+            else
+            {
+                FadePrimaryButton(m_BTN_Pressed_Color, 0);
+                FadePrimaryButtonText(m_TXT_Pressed_Color, 0);
+
+                m_SecondaryImage.sprite = null;
+                FadeSecondaryImage(Color.clear, 0);
+            }
+
+            m_CurrentState = State.Pressed;
+        }
+
+        public void ForceClear()
+        {
+            if (m_CurrentState != State.Normal && m_CurrentState == State.Pressed || m_CurrentState == State.Highlighted || m_CurrentState == State.IsInBetween)
+            {
+                m_CurrentState = State.Normal;
+
+                // Clear secondary
+                m_SecondaryImage.sprite = null;
+                FadeSecondaryImage(Color.clear, 0);
+
+                if (m_DayIsInCurrentCalenderMonth)
+                {
+                    FadePrimaryButton(m_BTN_Normal_Color, 0);
+                    FadePrimaryButtonText(m_TXT_Normal_Color, 0);
+                }
+                else
+                {
+                    FadePrimaryButton(m_BTN_Normal_FadedOut_Color, 0);
+                    FadePrimaryButtonText(m_TXT_Normal_FadedOut_Color, 0);
+                }
+            }
+        }
+
+        public void FadePrimaryButton(Color32 targetColor, float fadeDuration)
+        {
+            UITween.ForceColor(m_Primary_Image, targetColor, null, fadeDuration);
+        }
+
+        public void FadePrimaryButtonText(Color32 targetColor, float fadeDuration)
+        {
+            UITween.ForceColor(m_ButtonText, targetColor, null, fadeDuration);
+        }
+
+        public void FadeSecondaryImage(Color32 targetColor, float fadeDuration)
+        {
+            UITween.ForceColor(m_SecondaryImage, targetColor, null, fadeDuration);
+        }
+    }
+
+
+}
